39358bc9de1fc1ad7b95360f02611432a97171d9
iver fabian gonzalez vidal <ft.pumas@hotmail.com>
2016-01-27 06:32:04 -0600

    archivos sofom

---
+++ contacto.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+	<title>Soluciones y Oportunidades</title>
+	<meta  name = "viewport"  content = "width = dispositivo de ancho, escala inicial = 1, de máxima escala = 1, el usuario escalable = no">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+   <div  class = "container colorFondo " >
+ 
+       <div class= "row colorHeader" >
+       			
+       			<div class= "col-md-2 " ><img src="imagenes/logo.png" width="120" height="120" class="img-rounded"></div>
+       			<div class= "col-md-4">Soluciones y Oportunidades</div>
+       			<div class= "col-md-4">Soluciones y Oportinudades SA de CV SOFOM ENR</div>
+ 	   </div>
+    <div class="separador"></div>
+    <!--####### Menu ##### -->
+    <div class= "row colormenu"  >
+        
+        <div class="col-md-8 colormenu">
+        <div class="navbar-header">
+        	 <a class="navbar-brand" href="">Empresa</a>
+        </div>
+        	
+          	<ul class="nav navbar-nav">
+            	<li class="active"><a href="#">Productos Financieros</a></li>
+            	<li><a href="#">Informacion</a></li>
+            	<li><a href="#">Aviso de privacidad</a></li>
+            	<li><a href="#">Contacto</a></li>
+            	<li><a href="#">Galeria</a></li>
+
+            </ul>
+        </div>
+    </div>
+
+	<div class="container">
+	<form class="" action="" methos="POST">
+	<div class="form-group">
+	<div class="col-xs-4 well">
+			<input type="text" class="form-control separacion" id="nombre" name="txtNombre" placeholder="Nombre" required autofocus>		
+			<input type="text" class="form-control separacion" id="telefono" placeholder="Telefono" name="txtTelefono" >
+			<input type="email" class="form-control separacion" id="email" placeholder="E-mail" name="txtEmail" >
+			<textarea class="form-control hresize separacion" id="informacion" name="txtInformacion"></textarea>
+			<div class="col-xs-12">
+			<buttom class="btn btn-lg btn-primary btn-block btnSolicitar separacion" type="submit">Enviar</buttom>
+			</div>
+			</div>
+	</div>
+<div class="col-md-8">
+      
+
+       </div>
+
+       </div> 
+       <div class="separadorFooter"></div>
+
+       <!--#### pie de pagina ####--> 
+       <div class= "row footer" >
+       <div class="col-md-4">
+        <div class="Datos">
+         <div > Tel:(961) 618-1553 / (961) 618-1554 </div>
+         <div>Calle 12 Poniente Norte 931-C</div>
+         <div>Barrio Juy Juy</div>
+         <div>CP. 29038</div>
+         <div>Tuxtla Gutierrez, Chiapas, México.</div>
+        </div></div>
+        <div class="col-md-1">  </div>
+          <div class="col-md-7 Datos"> Siguenos:</div>
+          <div class="col-md-1"><img src="imagenes/GooglePlus.png" class="img-responsive"></div>
+          <div class="col-md-1"><img src="imagenes/facebooklogo.png" class="img-responsive"></div>
+          <div class="col-md-1"><img src="imagenes/twitter.jpg" class="img-responsive" ></div>
+         </div>
+          
+        	
+</div>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
